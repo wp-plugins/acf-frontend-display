26ebd88c855e5da0a23763f84824ef7740ec432b
gdurtan <gdurtan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-19 14:38:04 +0000

    Adding version 1.0.32 unmissing  hidden field set
    git-svn-id: https://plugins.svn.wordpress.org/acf-frontend-display/tags/1.0.32@1028649 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/fields-pack/field-poolAB-file.php
@@ -0,0 +1,399 @@
+<?php
+
+class acf_afd_pool_ab extends acf_field
+{
+
+		// vars
+	var $settings, // will hold info such as dir / path
+		$defaults; // will hold default field options
+		
+		
+	/*
+	*  __construct
+	*
+	*  Set name / label needed for actions / filters
+	*
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+	
+	function __construct()
+	{
+		// vars
+		$this->name = 'afd_pool_ab';
+		$this->label = __('FrontEnd Pool AB');
+		$this->category = __("Front End",'acf'); // Basic, Content, Choice, etc
+		$this->defaults = array(
+			// add default here to merge into your field. 
+			// This makes life easy when creating the field options as you don't need to use any if( isset('') ) logic. eg:
+			//'pool_ab' => 'thumbnail'
+		);
+		
+		
+		// do not delete!
+    	parent::__construct();
+    	
+    	
+    	// settings
+		$this->settings = array(
+			'path' => apply_filters('acf/helpers/get_path', __FILE__),
+			'dir' => apply_filters('acf/helpers/get_dir', __FILE__),
+			'version' => '1.0.0'
+		);
+
+	}
+	
+	
+	/*
+	*  create_options()
+	*
+	*  Create extra options for your field. This is rendered when editing a field.
+	*  The value of $field['name'] can be used (like below) to save extra data to the $field
+	*
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field	- an array holding all the field's data
+	*/
+	
+	function create_options( $field )
+	{
+		// defaults?
+		/*
+		$field = array_merge($this->defaults, $field);
+		*/
+		
+		// key is needed in the field names to correctly save the data
+		$key = $field['name'];
+		
+		
+		// Create Field Options HTML
+		?>
+		<tr class="field_option field_option_<?php echo $this->name; ?>">
+			<td class="label">
+				<label><?php _e("AB default value",'acf'); ?></label>
+				<p class="description"><?php _e("true or false value",'acf'); ?></p>
+			</td>
+		<td>
+		<?php
+		
+		do_action('acf/create_field', array(
+			'type'		=>	'checkbox',
+			'name'		=>	'fields['.$key.'][pool_ab]',
+			'value'		=>	$field['pool_ab'],
+			'layout'	=>	'horizontal',
+			'choices'	=>	array(
+				'true' => __('TAK (pole A)'),
+				'false' => __('Nie (pole B)'),
+			)
+		));
+		
+		?>
+	</td>
+</tr>
+		<?php
+		
+	}
+	
+	
+	/*
+	*  create_field()
+	*
+	*  Create the HTML interface for your field
+	*
+	*  @param	$field - an array holding all the field's data
+	*
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+	
+	function create_field( $field )
+	{
+		// defaults?
+		/*
+		$field = array_merge($this->defaults, $field);
+		*/
+		
+		// perhaps use $field['pool_ab'] to alter the markup?
+
+
+/*		echo '<pre>';
+		var_dump($field);
+		echo '</pre>';*/
+
+
+		$o = array( 'class', 'data-mask', 'data-type', 'name', 'placeholder');
+		foreach( $o as $k )
+		{
+			$e .= ' ' . $k . '="' . esc_attr( $field[ $k ] ) . '"';	
+		}
+		?>
+
+		<div class="switch-toggle switch-wpadmin">
+
+		  	
+		  
+		  <input id="<?php echo $field['id'];?>1" type="radio" value="true" <? echo $e; if($field['value'] == 'true'){ echo ' checked ';} ?>>
+		  <label for="<?php echo $field['id'];?>1" for="week" onclick="">YES</label>
+
+		  <input id="<?php echo $field['id'];?>2" type="radio" value="false" <? echo $e; if($field['value'] == 'false'){ echo ' checked ';} ?>>
+		  <label for="<?php echo $field['id'];?>2" for="month" onclick="">NO</label>
+
+		  <a></a>
+		</div>
+			
+
+
+	<?php
+	}
+	
+	
+	/*
+	*  input_admin_enqueue_scripts()
+	*
+	*  This action is called in the admin_enqueue_scripts action on the edit screen where your field is created.
+	*  Use this action to add CSS + JavaScript to assist your create_field() action.
+	*
+	*  $info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_enqueue_scripts
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function input_admin_enqueue_scripts()
+	{
+		// Note: This function can be removed if not used
+		
+		
+		// register ACF scripts
+		//wp_register_script( 'acf-input-afd_pool_ab', $this->settings['dir'] . 'js/input.js', array('acf-input'), $this->settings['version'] );
+		//wp_register_style( 'acf-input-afd_pool_ab', $this->settings['dir'] . 'css/input.css', array('acf-input'), $this->settings['version'] ); 
+		
+		
+		// scripts
+/*		wp_enqueue_script(array(
+			'acf-input-afd_pool_ab',	
+		));*/
+
+		// styles
+/*		wp_enqueue_style(array(
+			'acf-input-afd_pool_ab',	
+		));*/
+		
+		
+	}
+	
+	
+	/*
+	*  input_admin_head()
+	*
+	*  This action is called in the admin_head action on the edit screen where your field is created.
+	*  Use this action to add CSS and JavaScript to assist your create_field() action.
+	*
+	*  @info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_head
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function input_admin_head()
+	{
+		// Note: This function can be removed if not used
+	}
+	
+	
+	/*
+	*  field_group_admin_enqueue_scripts()
+	*
+	*  This action is called in the admin_enqueue_scripts action on the edit screen where your field is edited.
+	*  Use this action to add CSS + JavaScript to assist your create_field_options() action.
+	*
+	*  $info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_enqueue_scripts
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function field_group_admin_enqueue_scripts()
+	{
+		// Note: This function can be removed if not used
+	}
+
+	
+	/*
+	*  field_group_admin_head()
+	*
+	*  This action is called in the admin_head action on the edit screen where your field is edited.
+	*  Use this action to add CSS and JavaScript to assist your create_field_options() action.
+	*
+	*  @info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_head
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function field_group_admin_head()
+	{
+		// Note: This function can be removed if not used
+	}
+
+
+	/*
+	*  load_value()
+	*
+		*  This filter is applied to the $value after it is loaded from the db
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value - the value found in the database
+	*  @param	$post_id - the $post_id from which the value was loaded
+	*  @param	$field - the field array holding all the field options
+	*
+	*  @return	$value - the value to be saved in the database
+	*/
+	
+	function load_value( $value, $post_id, $field )
+	{
+		// Note: This function can be removed if not used
+		return $value;
+	}
+	
+	
+	/*
+	*  update_value()
+	*
+	*  This filter is applied to the $value before it is updated in the db
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value - the value which will be saved in the database
+	*  @param	$post_id - the $post_id of which the value will be saved
+	*  @param	$field - the field array holding all the field options
+	*
+	*  @return	$value - the modified value
+	*/
+	
+	function update_value( $value, $post_id, $field )
+	{
+		// Note: This function can be removed if not used
+		return $value;
+	}
+	
+	
+	/*
+	*  format_value()
+	*
+	*  This filter is applied to the $value after it is loaded from the db and before it is passed to the create_field action
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value	- the value which was loaded from the database
+	*  @param	$post_id - the $post_id from which the value was loaded
+	*  @param	$field	- the field array holding all the field options
+	*
+	*  @return	$value	- the modified value
+	*/
+	
+	function format_value( $value, $post_id, $field )
+	{
+		// defaults?
+		/*
+		$field = array_merge($this->defaults, $field);
+		*/
+		
+		// perhaps use $field['pool_ab'] to alter the $value?
+		
+		
+		// Note: This function can be removed if not used
+		return $value;
+	}
+	
+	
+	/*
+	*  format_value_for_api()
+	*
+	*  This filter is applied to the $value after it is loaded from the db and before it is passed back to the API functions such as the_field
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value	- the value which was loaded from the database
+	*  @param	$post_id - the $post_id from which the value was loaded
+	*  @param	$field	- the field array holding all the field options
+	*
+	*  @return	$value	- the modified value
+	*/
+	
+	function format_value_for_api( $value, $post_id, $field )
+	{
+		// defaults?
+		/*
+		$field = array_merge($this->defaults, $field);
+		*/
+		
+		// perhaps use $field['pool_ab'] to alter the $value?
+		
+		
+		// Note: This function can be removed if not used
+		return $value;
+	}
+	
+	
+	/*
+	*  load_field()
+	*
+	*  This filter is applied to the $field after it is loaded from the database
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field - the field array holding all the field options
+	*
+	*  @return	$field - the field array holding all the field options
+	*/
+	
+	function load_field( $field )
+	{
+		// Note: This function can be removed if not used
+		return $field;
+	}
+	
+	
+	/*
+	*  update_field()
+	*
+	*  This filter is applied to the $field before it is saved to the database
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field - the field array holding all the field options
+	*  @param	$post_id - the field group ID (post_type = acf)
+	*
+	*  @return	$field - the modified field
+	*/
+
+	function update_field( $field, $post_id )
+	{
+		// Note: This function can be removed if not used
+		return $field;
+	}
+	
+}
+	
+
+
+new acf_afd_pool_ab();
+
+?>
---
+++ trunk/readme.txt
@@ -0,0 +1,55 @@
+=== ACF Frontend display ===
+Contributors: gdurtan
+Tags: ACF, Advanced Custom Field, Frontend, display form
+Requires at least: 3.1
+Tested up to: 4.0
+Stable tag: 1.0.3
+License: GPLv2 or later
+
+ACF Frontend display - plugin to display ACF form on frontend your site
+
+== Description ==
+
+ACF Frontend display - checking option field to render your post or page on normal (frontend) display mode.
+
+Major features in ACF Frontend display include:
+
+* Automatically display ACF form on page or post.
+
+== Frequently Asked Questions ==
+
+No questions yet.
+
+== Installation ==
+
+Upload the ACF Frontend display plugin to your blog, Activate it.
+
+1, 2, 3: You're done!
+
+== Changelog ==
+
+= 1.0.3 =
+* Add frontend file upload field
+* Add afd_attached_forms_array() API method
+
+= 1.0.2 =
+* Fix JS errors with Conditional Logic
+* Add field poolAB
+* Add global frontend properties
+
+= 1.0.1 =
+* Add {afd} namespaces to variables and methods
+* Add plugin screenshots 
+* rename acf_frontend_form() and acf_form_permision() to afd_frontend_form() and afd_form_permision()
+
+= 1.0.0 =
+* Add metabox into pages and posts to activate ACF frontend form.
+* Add two extended API method: acf_frontend_form() and acf_form_permision() 
+* Add extra form to set arguments into acf_frontend_form()
+* Check link between ACF form and post. Is false message about it.
+
+== Screenshots ==
+
+1. Activate frontend display form
+2. More ACF form options
+
