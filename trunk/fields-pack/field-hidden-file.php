4b5557913322689037b86aa89d48b8b04e2e827d
gdurtan <gdurtan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-29 18:45:49 +0000

    tagging version 1.1.1
    git-svn-id: https://plugins.svn.wordpress.org/acf-frontend-display/tags/1.1.1@1123195 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/fields-pack/field-hidden-file.php
@@ -0,0 +1,432 @@
+<?php
+
+class acf_afd_hidden extends acf_field
+{
+
+		// vars
+	var $settings, // will hold info such as dir / path
+		$defaults; // will hold default field options
+		
+		
+	/*
+	*  __construct
+	*
+	*  Set name / label needed for actions / filters
+	*
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+	
+	function __construct()
+	{
+		// vars
+		$this->name = 'afd_hidden';
+		$this->label = __('Hidden');
+		$this->category = __("Front End",'acf'); // Basic, Content, Choice, etc
+		$this->defaults = array(
+			// add default here to merge into your field. 
+			// This makes life easy when creating the field options as you don't need to use any if( isset('') ) logic. eg:
+			//'pool_ab' => 'thumbnail'
+		);
+		
+		
+		// do not delete!
+    	parent::__construct();
+    	
+    	
+    	// settings
+		$this->settings = array(
+			'path' => apply_filters('acf/helpers/get_path', __FILE__),
+			'dir' => apply_filters('acf/helpers/get_dir', __FILE__),
+			'version' => '1.0.0'
+		);
+
+	}
+	
+	
+	/*
+	*  create_options()
+	*
+	*  Create extra options for your field. This is rendered when editing a field.
+	*  The value of $field['name'] can be used (like below) to save extra data to the $field
+	*
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field	- an array holding all the field's data
+	*/
+	
+	function create_options( $field )
+	{
+// vars
+		$key = $field['name'];
+
+		if ( is_admin() ) {
+		     $field_type = 'text';
+		} else {
+		     $field_type = 'hidden';
+		}
+		
+		?>
+	<tr class="field_option field_option_<?php echo $this->name; ?>">
+	<td class="label">
+		<label><?php _e("Default Value",'acf'); ?></label>
+		<p><?php _e("Appears when creating a new post",'acf') ?></p>
+	</td>
+	<td>
+		<?php 
+		do_action('acf/create_field', array(
+			'type'	=>	'text',
+			'name'	=>	'fields[' .$key.'][default_value]',
+			'value'	=>	$field['default_value'],
+		));
+		?>
+	</td>
+</tr>
+
+<tr class="field_option field_option_<?php echo $this->name; ?>">
+	<td class="label">
+		<label><?php _e("Forced hidden",'acf'); ?></label>
+		<p><?php _e("Field dont display and access on page, but storage data and send it to ACTIONS",'acf') ?></p>
+	</td>
+	<td>
+		<?php 
+		do_action('acf/create_field', array(
+			'type'	=>	'checkbox',
+			'name'	=>	'fields['.$key.'][forced_hidden]',
+			'value'	=>	$field['forced_hidden'],
+			'choices' => array(
+				'forced_hidden'	=>	__("Forced hidden",'acf'),
+				
+			)
+		));
+		?>
+	</td>	
+</tr>
+
+
+<tr class="field_option field_option_<?php echo $this->name; ?>">
+	<td class="label">
+		<label><?php _e("Form Message",'acf'); ?></label>
+		<p><?php _e("Replace ACF message with conditional logic",'acf') ?></p>
+	</td>
+	<td>
+
+		<?php 
+		do_action('acf/create_field', array(
+			'type'	=>	'textarea',
+			'name'	=>	'fields['.$key.'][updated_message]',
+			'value'	=>	$field['updated_message'],
+			'choices' => array(
+				'updated_message'	=>	__("updated_message",'acf'),
+				
+			)
+		));
+		?>
+	</td>
+</tr>
+
+<?php
+		
+	}
+	
+	
+	/*
+	*  create_field()
+	*
+	*  Create the HTML interface for your field
+	*
+	*  @param	$field - an array holding all the field's data
+	*
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+	
+	function create_field( $field )
+	{
+		// defaults?
+		/*
+		$field = array_merge($this->defaults, $field);
+		*/
+		
+		// perhaps use $field['pool_ab'] to alter the markup?
+
+
+		if ( is_admin() ) {
+		     $field_type = 'text';
+		} else {
+		     $field_type = 'hidden';
+		}
+	  
+		$o = array( 'class', 'data-mask', 'data-type', 'name', 'value' , 'placeholder');
+		foreach( $o as $k )
+		{
+			$e .= ' ' . $k . '="' . esc_attr( $field[ $k ] ) . '"';	
+		}
+		$field['value'] = $field['default_value'];
+		?>	
+		
+		 
+		<input id="<?php echo $field['id'];?>" type="<?php echo $field_type; ?>" name="<?php echo $field['name']?>" value="<?php echo $field['value']; ?>">
+		 
+
+	
+
+
+
+		
+
+
+	<?php
+	}
+	
+	
+	/*
+	*  input_admin_enqueue_scripts()
+	*
+	*  This action is called in the admin_enqueue_scripts action on the edit screen where your field is created.
+	*  Use this action to add CSS + JavaScript to assist your create_field() action.
+	*
+	*  $info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_enqueue_scripts
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function input_admin_enqueue_scripts()
+	{
+		// Note: This function can be removed if not used
+		
+		
+		// register ACF scripts
+		//wp_register_script( 'acf-input-afd_pool_ab', $this->settings['dir'] . 'js/input.js', array('acf-input'), $this->settings['version'] );
+		//wp_register_style( 'acf-input-afd_pool_ab', $this->settings['dir'] . 'css/input.css', array('acf-input'), $this->settings['version'] ); 
+		
+		
+		// scripts
+/*		wp_enqueue_script(array(
+			'acf-input-afd_pool_ab',	
+		));*/
+
+		// styles
+/*		wp_enqueue_style(array(
+			'acf-input-afd_pool_ab',	
+		));*/
+		
+		
+	}
+	
+	
+	/*
+	*  input_admin_head()
+	*
+	*  This action is called in the admin_head action on the edit screen where your field is created.
+	*  Use this action to add CSS and JavaScript to assist your create_field() action.
+	*
+	*  @info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_head
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function input_admin_head()
+	{
+		// Note: This function can be removed if not used
+	}
+	
+	
+	/*
+	*  field_group_admin_enqueue_scripts()
+	*
+	*  This action is called in the admin_enqueue_scripts action on the edit screen where your field is edited.
+	*  Use this action to add CSS + JavaScript to assist your create_field_options() action.
+	*
+	*  $info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_enqueue_scripts
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function field_group_admin_enqueue_scripts()
+	{
+		// Note: This function can be removed if not used
+	}
+
+	
+	/*
+	*  field_group_admin_head()
+	*
+	*  This action is called in the admin_head action on the edit screen where your field is edited.
+	*  Use this action to add CSS and JavaScript to assist your create_field_options() action.
+	*
+	*  @info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_head
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function field_group_admin_head()
+	{
+		// Note: This function can be removed if not used
+	}
+
+
+	/*
+	*  load_value()
+	*
+		*  This filter is applied to the $value after it is loaded from the db
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value - the value found in the database
+	*  @param	$post_id - the $post_id from which the value was loaded
+	*  @param	$field - the field array holding all the field options
+	*
+	*  @return	$value - the value to be saved in the database
+	*/
+	
+	function load_value( $value, $post_id, $field )
+	{
+		// Note: This function can be removed if not used
+		return $value;
+	}
+	
+	
+	/*
+	*  update_value()
+	*
+	*  This filter is applied to the $value before it is updated in the db
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value - the value which will be saved in the database
+	*  @param	$post_id - the $post_id of which the value will be saved
+	*  @param	$field - the field array holding all the field options
+	*
+	*  @return	$value - the modified value
+	*/
+	
+	function update_value( $value, $post_id, $field )
+	{
+		// Note: This function can be removed if not used
+		return $value;
+	}
+	
+	
+	/*
+	*  format_value()
+	*
+	*  This filter is applied to the $value after it is loaded from the db and before it is passed to the create_field action
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value	- the value which was loaded from the database
+	*  @param	$post_id - the $post_id from which the value was loaded
+	*  @param	$field	- the field array holding all the field options
+	*
+	*  @return	$value	- the modified value
+	*/
+	
+	function format_value( $value, $post_id, $field )
+	{
+		// defaults?
+		/*
+		$field = array_merge($this->defaults, $field);
+		*/
+		
+		// perhaps use $field['pool_ab'] to alter the $value?
+		
+		
+		// Note: This function can be removed if not used
+		return $value;
+	}
+	
+	
+	/*
+	*  format_value_for_api()
+	*
+	*  This filter is applied to the $value after it is loaded from the db and before it is passed back to the API functions such as the_field
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value	- the value which was loaded from the database
+	*  @param	$post_id - the $post_id from which the value was loaded
+	*  @param	$field	- the field array holding all the field options
+	*
+	*  @return	$value	- the modified value
+	*/
+	
+	function format_value_for_api( $value, $post_id, $field )
+	{
+		// defaults?
+		/*
+		$field = array_merge($this->defaults, $field);
+		*/
+		
+		// perhaps use $field['pool_ab'] to alter the $value?
+		
+		
+		// Note: This function can be removed if not used
+		return $value;
+	}
+	
+	
+	/*
+	*  load_field()
+	*
+	*  This filter is applied to the $field after it is loaded from the database
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field - the field array holding all the field options
+	*
+	*  @return	$field - the field array holding all the field options
+	*/
+	
+	function load_field( $field )
+	{
+		// Note: This function can be removed if not used
+		return $field;
+	}
+	
+	
+	/*
+	*  update_field()
+	*
+	*  This filter is applied to the $field before it is saved to the database
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field - the field array holding all the field options
+	*  @param	$post_id - the field group ID (post_type = acf)
+	*
+	*  @return	$field - the modified field
+	*/
+
+	function update_field( $field, $post_id )
+	{
+		// Note: This function can be removed if not used
+		return $field;
+	}
+	
+}
+	
+
+
+new acf_afd_hidden();
+
+?>
---
+++ trunk/inc/afd_acf_extend_api.php
@@ -0,0 +1,569 @@
+<?php
+function afd_form_permision( $options = array() )
+{
+    global $post;
+    
+    // filter post_id
+    $options['post_id'] = apply_filters('acf/get_post_id', $options['post_id'] );
+
+    // attributes
+    $options['form_attributes']['class'] .= 'acf-form';
+    
+    // register post box
+    if( empty($options['field_groups']) )
+    {
+        // get field groups
+        $filter = array(
+            'post_id' => $options['post_id']
+        );
+        
+    
+        $options['field_groups'] = array();
+        $options['field_groups'] = apply_filters( 'acf/location/match_field_groups', $options['field_groups'], $filter );
+    }
+
+    // html before fields
+    
+    $acfs = apply_filters('acf/get_field_groups', array());
+    
+
+
+    if( is_array($acfs) ){ foreach( $acfs as $acf ){
+        
+        //var_dump( $options['field_groups'] );
+        
+        // only add the chosen field groups
+        if( !in_array( $acf['id'], $options['field_groups'] ) )
+        {
+            continue;
+        }
+        return $options['field_groups'];
+    }}
+    // html after fields
+}
+
+function afd_attached_forms_array( $options = array() )
+{
+    global $post;
+    
+    // filter post_id
+    $options['post_id'] = apply_filters('acf/get_post_id', $options['post_id'] );
+    
+    // attributes
+    $options['form_attributes']['class'] .= 'acf-form';
+    
+    // register post box
+    if( empty($options['field_groups']) )
+    {
+        // get field groups
+        $filter = array(
+            'post_id' => $options['post_id']
+        );
+        
+    
+        $options['field_groups'] = array();
+        $options['field_groups'] = apply_filters( 'acf/location/match_field_groups', $options['field_groups'], $filter );
+    }
+
+    // html before fields
+    
+    $acfs = apply_filters('acf/get_field_groups', array());
+
+    if( is_array($acfs) ){ foreach( $acfs as $acf ){
+        
+        // only add the chosen field groups
+        if( !in_array( $acf['id'], $options['field_groups'] ) )
+        {
+             
+        }else{
+           $mapped_ids[] = $acf['id'];
+        }
+       
+    }
+     return $mapped_ids;
+}
+
+}
+
+function afd_form_head()
+{
+    // global vars
+    global $post_id;
+
+
+
+    
+    // verify nonce
+    if( isset($_POST['acf_nonce']) && wp_verify_nonce($_POST['acf_nonce'], 'input') )
+    {
+ 
+
+
+        // $post_id to save against
+        $post_id = $_POST['post_id'];
+        
+        
+        // allow for custom save
+        $post_id = apply_filters('acf/pre_save_post', $post_id);
+        
+        
+        // save the data
+        do_action('acf/save_post', $post_id);   
+
+
+        /* ADF + Forms Actions resoult */
+        if (defined('Forms_actions')) {
+        
+            $fa = call_user_func('fa_realize_form_actions'); 
+
+
+            if($fa["block_redirect"] != true){
+
+                if(isset($_POST['return']))
+                {
+                    
+                    var_dump('redirect disabled - afd_acf_extend_api.php');
+                    //wp_redirect($_POST['return']);
+                    exit;
+                }
+            }
+        }
+
+       
+
+        
+
+
+    }
+    
+    
+    // need wp styling
+    wp_enqueue_style(array(
+        'colors-fresh'
+    ));
+    
+        
+    // actions
+    //do_action('acf/input/admin_enqueue_scripts');
+
+    add_action('wp_head', 'acf_form_wp_head');
+    
+}
+
+function afd_frontend_form( $options = array() )
+{
+    global $post;
+    global $acf;
+
+
+    //var_dump('sad',$acf);
+    if(@$acf->AFD_block_display == true){
+        $block_display_guargian = true;
+    }
+
+    // defaults
+    $defaults = array(
+        'post_id' => false,
+        'field_groups' => array(),
+        'form' => true,
+        'form_attributes' => array(
+            'id' => 'post',
+            'class' => '',
+            'action' => '',
+            'method' => 'post',
+        ),
+        'return' => add_query_arg( 'updated', 'true', get_permalink() ),
+        'html_before_fields' => '',
+        'html_after_fields' => '',
+        'submit_value' => __("Update", 'acf'),
+        'updated_message' => __("Post updated", 'acf'), 
+    );
+    
+    
+    // merge defaults with options
+    $options = array_merge($defaults, $options);
+    
+    
+    // merge sub arrays
+    foreach( $options as $k => $v )
+    {
+        if( is_array($v) )
+        {
+            $options[ $k ] = array_merge($defaults[ $k ], $options[ $k ]);
+        }
+    }
+    
+    
+    // filter post_id
+    $options['post_id'] = apply_filters('acf/get_post_id', $options['post_id'] );
+    
+    
+    // attributes
+    $options['form_attributes']['class'] .= 'acf-form';
+    
+    $extra_data = json_decode( urldecode ( get_post_meta($options['post_id'],'_meta_afd_form_render_box_alpaca', true )), true );
+   
+    
+    // register post box
+    if( empty($options['field_groups']) )
+    {
+        // get field groups
+        $filter = array(
+            'post_id' => $options['post_id']
+        );
+        
+        
+/*        if( strpos($options['post_id'], 'user_') !== false )
+        {
+            $user_id = str_replace('user_', '', $options['post_id']);
+            $filter = array(
+                'ef_user' => $user_id
+            );
+        }
+        elseif( strpos($options['post_id'], 'taxonomy_') !== false )
+        {
+            $taxonomy_id = str_replace('taxonomy_', '', $options['post_id']);
+            $filter = array(
+                'ef_taxonomy' => $taxonomy_id
+            );
+        }*/
+        
+        
+        $options['field_groups'] = array();
+        $options['field_groups'] = apply_filters( 'acf/location/match_field_groups', $options['field_groups'], $filter );
+    }
+
+
+    // updated message
+    if(isset($_GET['updated']) && $_GET['updated'] == 'true' && $options['updated_message'])
+    {
+       $display_message = $options['updated_message'];
+    }
+   
+    
+    // display form
+
+    $html_f_head = '';
+
+    if( $options['form'] ): 
+    $html_f_head = '<form ';
+        if($options['form_attributes']){
+            foreach($options['form_attributes'] as $k => $v){
+                $html_f_head .= $k . '="' . $v .'" '; 
+        }
+    } 
+    $html_f_head .= '>';
+    endif; 
+
+    // html before fields
+    $html_body_top_decorator = '';
+    $html_body_top_decorator .= '<div id="poststuff">';
+    
+    $html_body_top_decorator .= $options['html_before_fields'];
+    
+    $acfs = apply_filters('acf/get_field_groups', array());
+    
+
+
+    if( is_array($acfs) ){ foreach( $acfs as $acf ){
+        
+
+
+        // only add the chosen field groups
+        if( !in_array( $acf['id'], $options['field_groups'] ) )
+        {
+            continue;
+        }
+
+        // load options
+        $acf['options'] = apply_filters('acf/field_group/get_options', array(), $acf['id']);
+        
+        // load fields
+                   
+        if (empty($fields)) {
+              $fields = apply_filters('acf/field_group/get_fields', array(), $acf['id']);
+
+        }else{
+              $fields = array_merge($fields,apply_filters('acf/field_group/get_fields', array(), $acf['id'])); 
+        }       
+
+       
+
+        
+
+        $html_body_top_decorator .= '<div id="acf_' . $acf['id'] . '" class="postbox acf_postbox ' . $acf['options']['layout'] . '">';
+        $html_body_top_decorator .= '<h3 class="hndle"><span>' . $acf['title'] . '</span></h3>';
+        $html_body_top_decorator .= '<div class="inside">';
+        
+        /* realize forced hidden fields  */
+        // nie trzeba tego robić - forcehidden działa :)
+        //$fields = forced_hidden($fields);
+        
+        /* ----------------------------- */
+        
+        /* ----------------------------- */
+        /* Change field class            */
+
+
+        if($options['display_template'] == 'Bootstrap'){
+            foreach ($fields as $key => $value) {
+                
+                
+                if(($value['type'] == 'checkbox')||($value['type'] == 'radio')){
+                   $fields[$key]['class'] = 'checkbox';
+                }else{
+                    $fields[$key]['class'] = 'form-control';
+                }
+
+                
+            }
+        }        
+
+        $html_body_bottom_decorator = '</div></div>';
+       
+            foreach ($fields as $field) {
+
+            if($field['updated_message'] != ''){
+                
+                $dependencyName = $field['conditional_logic']['rules'][0]["field"];
+                $dependencyValue = $field['conditional_logic']['rules'][0]["value"];
+               
+
+
+
+                if($_POST['fields'][$dependencyName] == $dependencyValue  ){
+                    //print('dep true');
+                    //$dependency_guardian = true;
+                    $display_message = $field['updated_message'];
+
+                }
+                /* is conditional logic */
+            };
+
+/*            if($dependency_guardian != true){
+                $display_message = $field['updated_message'];
+            }*/
+       
+    }        
+        
+    }}
+
+
+    // html after fields
+    $html_body_bottom_decorator .= $options['html_after_fields'];
+    
+    $submit_class = "";
+    if( $extra_data['display_template'] == 'Bootstrap'){ 
+        $submit_class = "btn btn-lg btn-success";
+    }
+    
+    $submit_left = "";
+   
+
+    if( $extra_data['label_placement'] == 'left'){ 
+        $submit_left_style = "margin-left:35%";
+        $submit_left = "afd_input_left";
+    }
+
+    if( $options['form'] ): 
+        $html_body_bottom_decorator .= '<!-- Submit -->';
+        $html_body_bottom_decorator .= '<div class="field '.$submit_left.'" style="'.$submit_left_style.'">';
+        $html_body_bottom_decorator .= '<input type="submit" value="'.$options['submit_value'].'" class="hvr-float-shadow '.$submit_class.'"/>';
+        $html_body_bottom_decorator .= '</div>';
+        $html_body_bottom_decorator .= '<!-- / Submit -->';
+    endif;
+    
+    $html_body_bottom_decorator .= '</div><!-- <div id="poststuff"> -->';
+    
+    if( $options['form'] ):
+        $html_body_bottom_decorator .= '</form>';
+    endif;
+    if($_POST['acf_nonce'] != ''){  
+        if($display_message != ''){
+            echo '<div class="message updated"><p>'.$display_message.'</p></div>';
+        }
+    }
+
+    if( $block_display_guargian != true ){
+
+        echo $html_f_head;
+        ?>
+        <div style="display:none">
+            <script type="text/javascript">
+                acf.o.post_id = <?php echo is_numeric($options['post_id']) ? $options['post_id'] : '"' . $options['post_id'] . '"'; ?>;
+            </script>
+            <input type="hidden" name="acf_nonce" value="<?php echo wp_create_nonce( 'input' ); ?>" />
+            <input type="hidden" name="post_id" value="<?php echo $options['post_id']; ?>" />
+            <input type="hidden" name="return" value="<?php echo $options['return']; ?>" />
+            <?php wp_editor('', 'acf_settings'); ?>
+        </div>
+        <?php
+        echo  $html_body_top_decorator;
+        /* ACF action problem - dont return data - only forced print it */  
+
+        // Add user profile data
+        if($_GET['guid'] != ''){
+            $current_user_id = get_current_user_id();
+            $user_info = get_userdata($_GET['guid']);
+            if($current_user_id == $_GET['guid']){
+                foreach ($fields as $key => $value) {
+                    if($value['name'] == 'first_name'){
+                        $fields[$key]['value'] = $user_info->first_name;
+                    }
+                    if($value['name'] == 'last_name'){
+                        $fields[$key]['value'] = $user_info->last_name;
+                    }
+                }
+            }else{
+                echo 'User acces lock. Login as right user !!!'; 
+            }
+        }
+
+
+        afd_create_fields($fields, $options['post_id']);
+       
+       
+
+        //do_action('acf/create_fields', $fields, $options['post_id']);
+        echo $html_body_bottom_decorator;
+    
+    }
+
+}
+
+function forced_hidden($fields){
+
+
+    $counter = 0;
+    foreach ($fields as $field) {
+
+        if( ( $field['forced_hidden'][0] == 'forced_hidden')  && ( $field['type'] = 'afd_hidden')){
+
+            //unset($fields[$counter]);
+            //update_field($field['key'], $field["value"], $post->ID);
+        }
+        $counter ++;
+        //afd_hidden
+        
+    }
+
+    return $fields;
+}
+
+function afd_create_fields( $fields, $post_id )
+    {
+        
+        $extra_data = json_decode( urldecode ( get_post_meta($post_id,'_meta_afd_form_render_box_alpaca', true )), true );
+        
+
+
+        if( is_array($fields) ){ foreach( $fields as $field ){
+            
+            // if they didn't select a type, skip this field
+            if( !$field || !$field['type'] || $field['type'] == 'null' )
+            {
+                continue;
+            }
+            
+            
+            // set value
+            if( !isset($field['value']) )
+            {
+                $field['value'] = apply_filters('acf/load_value', false, $post_id, $field);
+                $field['value'] = apply_filters('acf/format_value', $field['value'], $post_id, $field);
+            }
+            
+            
+            // required
+            $required_class = "";
+            $required_label = "";
+            
+            if( $field['required'] )
+            {
+                $required_class = ' required';
+                $required_label = ' <span class="required">*</span>';
+            }
+
+            if( $extra_data['display_template'] == 'Bootstrap'){ 
+                
+                $main_css_decorator = 'form-group';
+                if($field['type'] == 'radio'){
+                    $main_css_decorator = 'field radio';
+                }
+                if($field['type'] == 'checkbox'){
+                    $main_css_decorator = 'field checkbox';
+                }
+
+
+
+            }else{
+                $main_css_decorator = 'field';
+            }
+
+                //echo '<pre>';
+                //var_dump($field);
+                //echo '</pre>';
+
+            if( $extra_data['label_placement'] == 'left'){ 
+
+            
+                echo '<div id="acf-' . $field['name'] . '" class="'.$main_css_decorator.' field_type-' . $field['type'] . ' field_key-' . $field['key'] . $required_class . '" data-field_name="' . $field['name'] . '" data-field_key="' . $field['key'] . '" data-field_type="' . $field['type'] . '">';
+
+
+                    echo '<p class="label afd_label_left">';
+                        echo '<label for="' . $field['id'] . '">' . $field['label'] . $required_label . '</label>';
+                        echo $field['instructions'];
+                    echo '</p>';
+                    
+                    echo '<div class="afd_label_space">&nbsp;</div>';
+
+                    echo '<div class="field afd_input_left">';
+                    $field['name'] = 'fields[' . $field['key'] . ']';
+                    
+                    if( $extra_data['display_template'] == 'Bootstrap'){ 
+                        $exist_classes = $field['class'];
+                        if($field['type'] == 'checkbox'){
+                            $field['class'] = $exist_classes.' checkbox';
+                        }else{
+                           $field['class'] = $exist_classes.' form-control'; 
+                        }
+                    }
+                  
+                    do_action('acf/create_field', $field, $post_id);
+                    echo '</div>';
+
+                    echo '<br style="clear:both">';
+                echo '</div>';
+
+            }else{
+
+
+
+                echo '<div id="acf-' . $field['name'] . '" class="field '.$main_css_decorator.' field_type-' . $field['type'] . ' field_key-' . $field['key'] . $required_class . '" data-field_name="' . $field['name'] . '" data-field_key="' . $field['key'] . '" data-field_type="' . $field['type'] . '">';
+
+
+
+
+
+                    if( $extra_data['label_placement'] != 'none'){ 
+                        echo '<p class="label">';
+                            echo '<label for="' . $field['id'] . '">' . $field['label'] . $required_label . '</label>';
+                            echo $field['instructions'];
+                        echo '</p>';
+                    }
+
+
+                    
+                    $field['name'] = 'fields[' . $field['key'] . ']';
+                   
+                    do_action('acf/create_field', $field, $post_id);
+                
+                echo '</div>';
+
+            }
+            
+        }}
+                
+    }
+
+?>
